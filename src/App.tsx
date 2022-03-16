fa8f09c2830c07967fca1e649ee2ecf9024d464e
v4zha <arvin_j4848@protonmail.com>
2022-03-16 08:49:11 +0530

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import SignupFrom from './components/Signup'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <SignupFrom/>
     </div>
   )
 }
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from "formik";
-// import * as Yup from "yup";
-import  ApiServices from "../services/api-services";
-import {RegUsr} from "../types/Types";
-import { string } from 'yup';
+import { Formik, Field, Form} from "formik";
+import * as Yup from "yup";
+import ApiServices from "../services/api-services";
+import { RegUsr } from "../types/Types";
 
 /**
- * SignupForm 
+ * SignupForm with validation using SignupSchema
  * */
-const SignupForm=()=>{
+const SignupSchema=Yup.object().shape({
+    e_mail:Yup.string().email("Invalid Format :)").required('Email Required'),
+    passwd:Yup.string().min(8,"Password must have 8 char : )").required("Password Required"),
+});
+
+const SignupForm = () => {
     return (
         <Formik
             initialValues={{
-                uname:"",
-                passwd:"",
-                e_mail:"",
-            }}  
-            onSubmit={(values:RegUsr)=>{
-                console.log(values);
-                ApiServices.register(values.uname,values.e_mail,values.passwd);
-            }
-            }
+                uname: "",
+                passwd: "",
+                e_mail: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={(values: RegUsr, { setSubmitting }) => {
+                setTimeout(() => {
+                    console.log(`submitting form\n${values}`);
+                    ApiServices.register(values.uname, values.e_mail, values.passwd);
+                    setSubmitting(false);
+                }, 500);
+            }}
         >
-
-        </Formik>    
+            <Form>
+                <label htmlFor="uname">User Name</label>
+                <Field id="uname" name="uname"></Field>
+                <label htmlFor="passwd">Password</label>
+                <Field id="passwd" type="password" name="passwd"></Field>
+                <label htmlFor="e_mail">Email</label>
+                <Field id="e_mail" name="e_mail" ></Field>
+                <button type="submit">Submit</button>
+            </Form>
+        </Formik>
     );
 }
-
+export default SignupForm;
